3a3bbda9028a28fc0085d1158aa389328849647c
1vinicius1 <viniciuscunhasts@gmail.com>
2026-06-30 10:05:12 -0300

    Classe de configuração de segurança - permitindo funcionamento do H2

---
+++ src/main/java/com/cinema/crvg/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cinema.crvg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // Libera o acesso ao H2-Console e ao Swagger sem exigir login
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated() // Exige autenticação para as suas rotas do Cinema (POST, GET, etc)
+                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+                .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+}
